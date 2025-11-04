de7ed4ae1dd3127b5c1d9e6fee3b92ec92610554
lakr0003 <lakr0003@stud.kea.dk>
2025-11-04 14:40:32 +0100

    test2

--- src/app/components/Dot.jsx
+++ src/app/components/Dot.jsx
@@ -1,5 +1,10 @@
 const Dot = ({ color, select, clicked }) => {
-  return <button onClick={select} className={`dot dark active ${color} ${clicked}`} />;
+  return (
+    <button
+      onClick={select}
+      className={`dot dark active ${color} ${clicked}`}
+    />
+  );
 };
 
 export default Dot;
